59a42e3df43b67cb607fef2c08e0cc844c20d675
Mettel07 <mateushoffmann0707@gmail.com>
2025-10-07 01:15:14 -0300

    feat: refactor Post component layout for improved readability and structure

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,74 +26,76 @@ export function Post() {
             Publicado a 1h
           </time>
         </header>
-      </article>
-      <h1 className="mt-6 flex flex-col gap-6">
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p className="flex gap-1 focus:outline-1 focus:outline-green-500">
-          👉
-          <a
-            href="#"
-            className="text-green-500 font-bold hover:text-green-300 
+
+        <div className="mt-6 flex flex-col gap-6">
+          <p>Fala galeraa 👋</p>
+          <p>
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
+            🚀
+          </p>
+          <p className="flex gap-1 focus:outline-1 focus:outline-green-500">
+            👉
+            <a
+              href="#"
+              className="text-green-500 font-bold hover:text-green-300 
             focus:outline-1 focus:outline-green-500"
-          >
-            github.com/mateushoffmann7
-          </a>
-        </p>
-        <p className="flex gap-1">
-          <a
-            href="#"
-            className="text-green-500 font-bold hover:text-green-300 
+            >
+              github.com/mateushoffmann7
+            </a>
+          </p>
+          <p className="flex gap-1">
+            <a
+              href="#"
+              className="text-green-500 font-bold hover:text-green-300 
             focus:outline-1 focus:outline-green-500"
-          >
-            #novoprojeto
-          </a>
-          <a
-            href="#"
-            className="text-green-500 font-bold hover:text-green-300 
+            >
+              #novoprojeto
+            </a>
+            <a
+              href="#"
+              className="text-green-500 font-bold hover:text-green-300 
             focus:outline-1 focus:outline-green-500"
-          >
-            #nlw
-          </a>
-          <a
-            href="#"
-            className="text-green-500 font-bold hover:text-green-300 
+            >
+              #nlw
+            </a>
+            <a
+              href="#"
+              className="text-green-500 font-bold hover:text-green-300 
             focus:outline-1 focus:outline-green-500"
-          >
-            #rocketseat
-          </a>
-        </p>
-        <form className="flex flex-col items-start">
-          <strong
-            className="border-t-1 border-gray-600 w-full py-[1rem] leading-relaxed
+            >
+              #rocketseat
+            </a>
+          </p>
+          <form className="flex flex-col items-start">
+            <strong
+              className="border-t-1 border-gray-600 w-full py-[1rem] leading-relaxed
             focus:outline-1 focus:outline-green-500"
-          >
-            Deixe seu feedback
-          </strong>
-          <div className="group w-full">
-            <textarea
-              placeholder="Deixe um comentário"
-              className="border-none focus:outline-1 focus:outline-green-500
+            >
+              Deixe seu feedback
+            </strong>
+            <div className="group w-full">
+              <textarea
+                placeholder="Deixe um comentário"
+                className="border-none focus:outline-1 focus:outline-green-500
               rounded-md w-full h-24 p-4 bg-gray-900 resize-none leading-relaxed"
-            />
-            <footer className="hidden group-focus-within:block">
-              <button
-                type="submit"
-                className="mt-4 bg-green-500 text-white rounded-[8px]
+              />
+              <footer className="hidden group-focus-within:block">
+                <button
+                  type="submit"
+                  className="mt-4 bg-green-500 text-white rounded-[8px]
                      h-[50px] py-4 px-[1.5rem] font-bold flex
                      items-center justify-center gap-2 cursor-pointer
                     hover:bg-green-300 transition-colors 
                     focus:outline-1 focus:outline-green-300"
-              >
-                Publicar
-              </button>
-            </footer>
-          </div>
-        </form>
-      </h1>
+                >
+                  Publicar
+                </button>
+              </footer>
+            </div>
+          </form>
+        </div>
+      </article>
     </div>
   );
 }
