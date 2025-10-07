6ae06c2373f8f1271817741c2a1b5638a598d2ca
Mettel07 <mateushoffmann0707@gmail.com>
2025-10-07 00:45:07 -0300

    feat: update App component structure for improved layout and readability

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,20 @@ import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div
+      className="min-h-screen bg-gray-900 text-gray-300
+        text-base font-[roboto] antialiased"
+    >
+      <Header />
       <div
-        className="bg-gray-900 text-gray-300 h-full
-      text-base font-[roboto] antialiased"
-      >
-        <Header />
-        <div
-          className="max-w-[70rem] my-[2rem] mx-auto py-0 px-[1rem]
+        className="max-w-[70rem] my-[2rem] mx-auto py-0 px-[1rem]
         grid grid-cols-[256px_1fr] gap-[2rem] items-start"
-        >
-          <Sidebar />
-          <main>
-            <Post />
-            <Post />
-          </main>
-        </div>
+      >
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
